cf3d4a22ac43d4b3e90e0b28d23f1ac3e6f8f556
jithu <webdeveloper.jithu@gmail.com>
2023-11-10 22:07:21 +0530

    added edit|remove features | ui optimized

--- src/lib/components/task.svelte
+++ src/lib/components/task.svelte
@@ -4,8 +4,17 @@
 
 	export let todo: TODO;
 	export let isOpen: boolean;
+	let editMode: boolean = false;
+	let editId: number = 0;
 
 	let todoDesc = '';
+
+	function scrollToBottom() {
+		let container = document.getElementById('dt-todo-container');
+		//@ts-ignore
+		container.scrollTop = container.scrollHeight;
+  	}
+
 	const addTodo = () => {
 		if (!todoDesc) return
         const todos = todo.tasks;
@@ -14,7 +23,21 @@
 
         todo.tasks = [...todos, { id: lastId + 1, desc: todoDesc, isDone: false }]
         todoDesc = ''
+		scrollToBottom();
 	};
+
+	const updateTodo = () => {
+		const index = todo.tasks.findIndex(todo => todo.id === editId)
+		todo.tasks[index].desc = todoDesc;
+		todoDesc = '';
+		editMode = false;
+		editId = 0;
+	}
+
+	const removeTask = () => {
+		const index = todo.tasks.findIndex(todo => todo.id === editId)
+		todo.tasks.splice(index, 1);
+	}
 
 	const toggleCompleted = (e: MouseEvent): void => {
 		//@ts-ignore
@@ -39,22 +62,72 @@
 			{#each todo.tasks as task, i (task.id)}
 				<li class="border rounded-lg border-gray-500 p-2 my-5 {task.isDone ? 'done' : 'not-done'}">
 					<div class="form-control">
-						<label class="cursor-pointer label">
+						<label class="cursor-pointer label flex items-center">
 							<span class="label-text p-2">{task.desc}</span>
+							<div class="flex items-center">
 							<input
 								type="checkbox"
 								on:click={(e) => toggleCompleted(e)}
 								value={task.id}
 								class="checkbox checkbox-accent"
 							/>
+							<button class="btn" on:click={() => {
+								editMode = true;
+								editId = task.id;
+								todoDesc = task.desc;
+							}}>
+								<svg
+									width="17"
+									height="17"
+									viewBox="0 0 24 24"
+									fill="none"
+									xmlns="http://www.w3.org/2000/svg"
+									>
+									<path
+										fill-rule="evenodd"
+										clip-rule="evenodd"
+										d="M21.2635 2.29289C20.873 1.90237 20.2398 1.90237 19.8493 2.29289L18.9769 3.16525C17.8618 2.63254 16.4857 2.82801 15.5621 3.75165L4.95549 14.3582L10.6123 20.0151L21.2189 9.4085C22.1426 8.48486 22.338 7.1088 21.8053 5.99367L22.6777 5.12132C23.0682 4.7308 23.0682 4.09763 22.6777 3.70711L21.2635 2.29289ZM16.9955 10.8035L10.6123 17.1867L7.78392 14.3582L14.1671 7.9751L16.9955 10.8035ZM18.8138 8.98525L19.8047 7.99429C20.1953 7.60376 20.1953 6.9706 19.8047 6.58007L18.3905 5.16586C18 4.77534 17.3668 4.77534 16.9763 5.16586L15.9853 6.15683L18.8138 8.98525Z"
+										fill="currentColor"
+									/>
+									<path
+										d="M2 22.9502L4.12171 15.1717L9.77817 20.8289L2 22.9502Z"
+										fill="currentColor"
+									/>
+									</svg>
+							</button>
+							<button class="btn" on:click={() => {
+								editId = task.id
+								removeTask();
+							}}>
+								<svg
+									width="24"
+									height="24"
+									viewBox="0 0 24 24"
+									fill="none"
+									xmlns="http://www.w3.org/2000/svg"
+									>
+									<path
+										fill-rule="evenodd"
+										clip-rule="evenodd"
+										d="M17 5V4C17 2.89543 16.1046 2 15 2H9C7.89543 2 7 2.89543 7 4V5H4C3.44772 5 3 5.44772 3 6C3 6.55228 3.44772 7 4 7H5V18C5 19.6569 6.34315 21 8 21H16C17.6569 21 19 19.6569 19 18V7H20C20.5523 7 21 6.55228 21 6C21 5.44772 20.5523 5 20 5H17ZM15 4H9V5H15V4ZM17 7H7V18C7 18.5523 7.44772 19 8 19H16C16.5523 19 17 18.5523 17 18V7Z"
+										fill="currentColor"
+									/>
+									<path d="M9 9H11V17H9V9Z" fill="currentColor" />
+									<path d="M13 9H15V17H13V9Z" fill="currentColor" />
+									</svg>
+							</button>
+						</div>
 						</label>
+						
 					</div>
 				</li>
 			{/each}
 		</ul>
 		<div class="dt-todo-input-container mt-3">
 			<textarea class="textarea textarea-bordered" bind:value={todoDesc} />
-			<button class="btn btn-primary" on:click={addTodo}>Add</button>
+			<button class="btn btn-primary" on:click={editMode ? updateTodo : addTodo}>
+				{ editMode ? 'Update' : 'Add' }
+			</button>
 		</div>
 	</div>
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -25,12 +25,13 @@
 
 </script>
 
-<section class="dt-todo-container container max-w-screen-sm mx-auto px-3 py-3" data-theme="cupcake">
-	<h2 class="text-2xl font-bold mt-5">DateTask App</h2>
+<section id="dt-todo-container" class="dt-todo-container container max-w-screen-sm mx-auto px-3 py-3 max-h-screen overflow-y-auto" data-theme="cupcake">
+	<h2 class="text-2xl font-bold mt-5 text-center">DateTask App</h2>
 	<div class="divider" />
 	<div class="dt-todo-task-container">
 		{#each todos as todo, i (todo.id)}
 			<TaskComponent {todo} isOpen={i === todoCount - 1} />
 		{/each}
 	</div>
+	<p class="text-right pt-10">With Love <b>Jithu TJ</b></p>
 </section>
